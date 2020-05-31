a9e303595b0005212c530fde3dd2bc6d817d1bea
k <k@aura.top>
2020-05-31 18:47:44 +0300

    Updates

---
+++ precache-manifest.e5f4d6d64ca03bb3058126187bf9d56a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133f4c0cc7f157010e30513373ad00df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "699013f3d33584152b9f",
+    "url": "/static/css/main.d8ebfcae.chunk.css"
+  },
+  {
+    "revision": "b7621f1fc28b6b1aeae0",
+    "url": "/static/js/2.1ab9e0c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1ab9e0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699013f3d33584152b9f",
+    "url": "/static/js/main.093bc109.chunk.js"
+  },
+  {
+    "revision": "68ce36deed8bdd796b7e",
+    "url": "/static/js/runtime-main.c686ea62.js"
+  }
+]);
