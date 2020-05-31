a4ad8f4039c5afa4a7cbde0819e51fd111d8de67
k <k@aura.top>
2020-05-31 19:28:23 +0300

    wip

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  from 'react';
 import './index.css';
 import Scene from '../Scene';
 import { noop } from '../../utils';
--- src/components/Scene/index.js
+++ src/components/Scene/index.js
@@ -4,6 +4,7 @@ import range from 'lodash.range';
 import { emptyStyles, randStyles } from './styles';
 import Planet from './Planet';
 import { rand } from '../../utils';
+import { ReactComponent as IconLike } from '../../static/icons/heart.svg';
 
 const PLANETS_COUNT = 11;
 const PLANETS_INDEX = range(0, PLANETS_COUNT);
@@ -11,27 +12,42 @@ const PLANETS_INDEX = range(0, PLANETS_COUNT);
 const PLANETS_VISIBLE_MIN = 2;
 const PLANETS_VISIBLE_MAX = 6;
 
+const SCENES_MAX = 100;
+
+const randScene = () => {
+  const visibleCount = rand(PLANETS_VISIBLE_MIN, PLANETS_VISIBLE_MAX);
+  const planets = PLANETS_INDEX.map((index) => index < visibleCount ? randStyles() : emptyStyles());
+  console.log('generate scene', { visibleCount, planets });
+  return planets;
+};
+const INITIAL_SCENE = randScene();
+
 const Scene = () => {
-  const [iteration, setIteration] = useState(0);
-  const nextIteration = useCallback(() => setIteration(iteration + 1), [iteration, setIteration]);
+  const [scenes, setScenes] = useState([INITIAL_SCENE]);
+  const [sceneIndex, setSceneIndex] = useState(0);
 
-  const onLike = useCallback(() => nextIteration(), [nextIteration]);
-  const onDislike = useCallback(() => nextIteration(), [nextIteration]);
+  const nextScene = useCallback(() => {
+    if (scenes[sceneIndex + 1] === undefined) {
+      setScenes([ ...scenes, randScene()].slice(-SCENES_MAX));
+    }
+    setSceneIndex(Math.min(SCENES_MAX - 1, sceneIndex + 1));
+  }, [sceneIndex, setSceneIndex, scenes, setScenes]);
 
-  const planets = useMemo(() => {
-    const visibleCount = rand(PLANETS_VISIBLE_MIN, PLANETS_VISIBLE_MAX);
-    const styles = PLANETS_INDEX.map((index) => index < visibleCount ? randStyles() : emptyStyles());
-    console.log('generate planets', { iteration, visibleCount, styles });
-    return styles;
-  }, [iteration]);
+  const prevScene = useCallback(() => {
+    setSceneIndex(Math.max(0, sceneIndex - 1));
+  }, [sceneIndex, setSceneIndex, scenes.length]);
 
+  const onLike = useCallback(() => nextScene(), [nextScene]);
+
+  const planets = scenes[sceneIndex];
   return <div className={'scene'}>
     <div className={'frame'}>
       {planets.map((planet, index) => <Planet key={index} id={index} styles={planet} />)}
     </div>
     <div className={'actions'}>
-      <button onClick={onDislike}>- dislike</button>
-      <button onClick={onLike}>like +</button>
+      <button onClick={prevScene} disabled={sceneIndex === 0}>&larr; prev</button>
+      <button onClick={onLike}><IconLike /></button>
+      <button onClick={nextScene}>next &rarr;</button>
     </div>
   </div>
 }
